f3f4b524b5a928af4442db2c7eb144333ad8558d
RoaringDragon2 <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-12 16:43:09 -0700

    Add project to projects page

--- src/views/Pages/Projects/Project/Project.js
+++ src/views/Pages/Projects/Project/Project.js
@@ -4,6 +4,7 @@ import './Project.css';
 function Project(props) {
   return (
     <div className={"Project " + props.className}>
+      <h1>{props.title}</h1>
       {props.children}
     </div>
   );
--- src/views/Pages/Projects/Projects.js
+++ src/views/Pages/Projects/Projects.js
@@ -5,8 +5,10 @@ import Project from './Project/Project'
 function Projects() {
   return (
     <div className="Projects">
-      <Project className="left">
-        This is the first project
+      <Project className="left" title="alexanderfranklin.net">
+        This is the website you are on. It is
+        built using react, and it is hosted on AWS.
+        I took the background photo myself.
       </Project>
       <Project className="right">
         This is the second project
