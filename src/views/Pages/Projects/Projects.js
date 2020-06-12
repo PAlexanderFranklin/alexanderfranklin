3a8a6139fb01efc7637a09ecf0ff6da4bf71c0b0
RoaringDragon2 <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-11 20:36:17 -0700

    Content

--- src/views/Pages/Projects/Projects.js
+++ src/views/Pages/Projects/Projects.js
@@ -4,7 +4,7 @@ import './Projects.css';
 function Projects() {
   return (
     <div className="Projects">
-      I have things on my <a href="https://github.com/PAlexanderFranklin">GitHub</a>
+      I have some projects on my <a href="https://github.com/PAlexanderFranklin">GitHub</a>
     </div>
   );
 }
