543291a74a36b5a0829212df6fdb2d32145dd1bf
RoaringDragon2 <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-11 20:25:28 -0700

    Add content

--- src/views/Pages/Home/Home.js
+++ src/views/Pages/Home/Home.js
@@ -10,7 +10,7 @@ function Home() {
         Really though, it means I spend lot of time googling things.
       </p>
       <p>
-        I have a lot of related interests too. I like to code no matter the medium, <br />
+        I don't just build websites. I like to code no matter the medium, <br />
         be it Python, or a visual language like scratch, or even a video game
         like <a href="http://www.zachtronics.com/tis-100/">TIS-100</a>
       </p>
--- src/views/Pages/Projects/Projects.js
+++ src/views/Pages/Projects/Projects.js
@@ -4,7 +4,7 @@ import './Projects.css';
 function Projects() {
   return (
     <div className="Projects">
-      <a href="https://github.com/PAlexanderFranklin">GitHub</a>
+      I have things on my <a href="https://github.com/PAlexanderFranklin">GitHub</a>
     </div>
   );
 }
