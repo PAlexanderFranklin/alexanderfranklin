bd9e3e97a59414cf745d31e4b9b3b239836d8761
RoaringDragon2 <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-11 19:30:56 -0700

    Add Resume PDF

--- src/views/Pages/Resume/Resume.js
+++ src/views/Pages/Resume/Resume.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Resume.css';
+import pdf from './Resume.pdf'
 
 function Resume() {
   return (
     <div className="Resume">
+      <iframe src={pdf} height="1150rem" width="850rem" />
     </div>
   );
 }
