85f0eb738a7346956613bf31502b6c8376e49682
RoaringDragon2 <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-13 16:23:21 -0700

    Resume stuff

--- src/views/Pages/Resume/Resume.js
+++ src/views/Pages/Resume/Resume.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './Resume.css';
-import pdf from './Resume.pdf'
 
 function Resume() {
   return (
     <div className="Resume">
-      <iframe title="resume" src={pdf} height="1150rem" width="850rem" />
+      <div>
+        <h1>Technical Skills</h1>
+        <b>Languages:</b> Javascript, HTML5, CSS, Python, Applescript. <br />
+        <b>Frameworks and Technologies</b> React, Redux, Git Version Control, AWS(amplify), Node.js
+      </div>
+      <div>
+        <h1>Education</h1>
+        <b>Arizona State University Bachelor of Science in Software Engineering: Est. 2024</b><hr />
+        <b>SAT</b>
+        <ul>
+          <li>Score: 1390</li>
+          <li>94th percentile</li>
+        </ul>
+        <b>High School Diploma</b>
+        <ul>
+          <li>Homeschool</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Work Experience</h1>
+      </div>
     </div>
   );
 }
