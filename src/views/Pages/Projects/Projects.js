0395f755cea18c78acd2d8d7ffeec43902094dd1
RoaringDragon2 <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-14 16:45:08 -0700

    Describe scratch project

--- src/views/Pages/Projects/Projects.js
+++ src/views/Pages/Projects/Projects.js
@@ -11,6 +11,12 @@ function Projects() {
         I took the background photo myself.
       </Project>
       <Project className="right wide" title="Gravity Rotation Simulator">
+        I made this using scratch, a visual block language. It uses its own coordinate system,
+        and decides when and where to render objects to the scratch system. It has velocity, acceleration, and
+        basic air resistence. It also has basic collision detection. <hr/>
+        Press the green flag to start. The map takes some time to load. <br/>
+        WAD to move and jump <br/>
+        QSE to rotate/flip gravity <br/>
         <iframe 
           title="Gravity"
           src="https://scratch.mit.edu/projects/212668429/embed" 
