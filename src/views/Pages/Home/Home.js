eceed062c9935b8bb768def5d5213abae57e3528
RoaringDragon2 <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-13 20:03:40 -0700

    Add to resume Format line breaks Add bottom margin to content

--- src/views/Pages/Home/Home.js
+++ src/views/Pages/Home/Home.js
@@ -6,11 +6,11 @@ function Home() {
     <div className="Home">
       <h1>My name is Alexander Franklin.</h1>
       <p>
-        I am a web developer, which means I create websites like this. <br />
+        I am a web developer, which means I create websites like this. <br/>
         Really though, it means I spend lot of time googling things.
       </p>
       <p>
-        I don't just build websites. I like to code no matter the medium, <br />
+        I don't just build websites. I like to code no matter the medium, <br/>
         be it Python, or a visual language like scratch, or even a video game
         like <a href="http://www.zachtronics.com/tis-100/">TIS-100</a>
       </p>
--- src/views/Pages/Resume/Resume.js
+++ src/views/Pages/Resume/Resume.js
@@ -6,24 +6,29 @@ function Resume() {
     <div className="Resume">
       <div>
         <h1>Technical Skills</h1>
-        <b>Languages:</b> Javascript, HTML5, CSS, Python, Applescript. <br />
+        <b>Languages:</b> Javascript, HTML5, CSS, Python, Applescript. <br/>
         <b>Frameworks and Technologies</b> React, Redux, Git Version Control, AWS(amplify), Node.js
       </div>
       <div>
         <h1>Education</h1>
-        <b>Arizona State University Bachelor of Science in Software Engineering: Est. 2024</b><hr />
+        <b>Arizona State University Bachelor of Science in Software Engineering: Est. 2024</b><hr/>
         <b>SAT</b>
         <ul>
           <li>Score: 1390</li>
           <li>94th percentile</li>
         </ul>
-        <b>High School Diploma</b>
+        <hr/>
+        <b>High School Diploma</b> <span className="date">Apr 2020</span>
         <ul>
           <li>Homeschool</li>
         </ul>
       </div>
       <div>
         <h1>Work Experience</h1>
+        <b>Harvest Homes</b> <span className="date">Sep 2018-Present</span> <br/>
+        <ul>
+          <li>Maintenance</li>
+        </ul>
       </div>
     </div>
   );
