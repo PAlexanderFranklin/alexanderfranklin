19250e1dbfb6e333be774ddd295a83826e5377b5
RoaringDragon2 <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-13 20:14:16 -0700

    Remove link on home

--- src/views/Pages/Home/Home.js
+++ src/views/Pages/Home/Home.js
@@ -11,8 +11,7 @@ function Home() {
       </p>
       <p>
         I don't just build websites. I like to code no matter the medium, <br/>
-        be it Python, or a visual language like scratch, or even a video game
-        like <a href="http://www.zachtronics.com/tis-100/">TIS-100</a>
+        be it Python, or a visual language like scratch, or even a video game.
       </p>
     </div>
   );
