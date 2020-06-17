5fdb8107769d23beef8fe406e7560678ef1756c9
RoaringDragon2 <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-16 17:09:52 -0700

    Add State Demo

--- src/views/Pages/Projects/Projects.js
+++ src/views/Pages/Projects/Projects.js
@@ -1,34 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Projects.css';
 import Project from './Project/Project'
 
 function Projects() {
+  let projectArray = [];
+
+  projectArray.push(
+    <Project className="left" title="alexanderfranklin.net">
+      This is the website you are on. It was
+      built using react, and it is hosted on AWS.
+    </Project>
+  );
+
+  const [bars, setBars] = useState(1);
+  if (bars < 0) {
+    setBars(0);
+  }
+  if (bars > 6) {
+    setBars(6);
+  }
+  let barArray = [];
+  for (let i = 1; i < 7; i++) {
+    let active = "active_bar";
+    if (i > bars) {
+      active = "";
+    }
+    barArray.push(<button onClick={() => setBars(i)} className={"bar " + active} style={{height: i + "em"}} />)
+  }
+  projectArray.push(
+    <Project className="right" title="This demonstrates state.">
+      <div className="bar_display">
+        <button onClick={() => setBars(bars - 1)} className="arrow_button"><h1>{'<'}</h1></button>
+        {barArray}
+        <button onClick={() => setBars(bars + 1)} className="arrow_button"><h1>{'>'}</h1></button>
+      </div>
+    </Project>
+  );
+
+  projectArray.push(
+    <Project className="left wide" title="Gravity Rotation Simulator">
+      I made this using scratch, a visual block language. It uses its own coordinate system,
+      and decides when and where to render objects to the scratch system. It has velocity, acceleration, and
+      basic air resistence. It also has basic collision detection. <hr/>
+      Press the green flag to start. The map takes some time to load. <br/>
+      WAD to move and jump <br/>
+      QSE to rotate/flip gravity <br/>
+      <iframe 
+        title="Gravity"
+        src="https://scratch.mit.edu/projects/212668429/embed" 
+        allowtransparency="true"
+        width="485" height="402" 
+        frameborder="0" 
+        scrolling="no" 
+        allowfullscreen>
+      </iframe>
+    </Project>
+  );
+
   return (
     <div className="Projects">
-      <Project className="left" title="alexanderfranklin.net">
-        This is the website you are on. It was
-        built using react, and it is hosted on AWS.
-      </Project>
-      <Project className="right wide" title="Gravity Rotation Simulator">
-        I made this using scratch, a visual block language. It uses its own coordinate system,
-        and decides when and where to render objects to the scratch system. It has velocity, acceleration, and
-        basic air resistence. It also has basic collision detection. <hr/>
-        Press the green flag to start. The map takes some time to load. <br/>
-        WAD to move and jump <br/>
-        QSE to rotate/flip gravity <br/>
-        <iframe 
-          title="Gravity"
-          src="https://scratch.mit.edu/projects/212668429/embed" 
-          allowtransparency="true"
-          width="485" height="402" 
-          frameborder="0" 
-          scrolling="no" 
-          allowfullscreen>
-        </iframe>
-      </Project>
-      <Project className="left" title="This demonstrates state.">
-        not yet.
-      </Project>
+      {projectArray}
     </div>
   );
 }
