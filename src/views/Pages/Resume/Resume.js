f886df82d20f287f7ea0659f1dc57d1fab7d00de
RoaringDragon2 <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-17 17:11:21 -0700

    Update Resume

--- src/views/Pages/Resume/Resume.js
+++ src/views/Pages/Resume/Resume.js
@@ -4,15 +4,16 @@ import './Resume.css';
 function Resume() {
   return (
     <div className="Resume">
+      Alexander Franklin <span className="date">Portland, Oregon</span>
       <div>
         <h1>Technical Skills</h1>
         <b>Languages:</b> Javascript, HTML5, CSS, Python, Applescript. <br/>
-        <b>Frameworks and Technologies</b> React, Redux, Git Version Control, AWS(amplify), Node.js
+        <b>Frameworks and Technologies</b> React, Redux, Git Version Control, AWS(Amplify), Node.js
       </div>
       <div>
         <h1>Education</h1>
         <b>Arizona State University Bachelor of Science in Software Engineering: Est. 2024</b><hr/>
-        <b>SAT</b>
+        <b>SAT</b> <span className="date">Mar 2019</span>
         <ul>
           <li>Score: 1390</li>
           <li>94th percentile</li>
