1bf1419f231623afd7796ecc47ae5bb6147456d7
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-29 23:16:11 -0700

    Move contact info to home page

--- src/views/Pages/Home/Home.js
+++ src/views/Pages/Home/Home.js
@@ -4,19 +4,28 @@ import './Home.css';
 
 function Home() {
   return (
-    <div className="Home">
-      <h1>My name is Alexander Franklin.</h1>
-      <p>
-        I am a web developer, which means I create websites like this. <br/>
-        It also means I google things a lot.
-      </p>
-      <p>
-        I don't just build websites. I like to code no matter the medium, <br/>
-        be it Python, or a visual language like scratch, or even a video game.
-      </p>
-      <p>
-        Read more about this website on the <Link to="/projects">projects</Link> page.
-      </p>
+    <div className="content">
+      <div className="Home bubble">
+        <h1>My name is Alexander Franklin.</h1>
+        <p>
+          I am a web developer, which means I create websites like this. <br/>
+          It also means I google things a lot.
+        </p>
+        <p>
+          I don't just build websites. I like to code no matter the medium, <br/>
+          be it Python, or a visual language like scratch, or even a video game.
+        </p>
+        <p>
+          Read more about this website on the <Link to="/projects">projects</Link> page.
+        </p>
+      </div>
+      <div className="Contact bubble">
+        <h1>How to contact me</h1>
+        <p>
+          <b>Email:</b> PAlexanderFranklin@gmail.com <br />
+          <b>Phone:</b> 509-930-9363
+        </p>
+      </div>
     </div>
   );
 }
