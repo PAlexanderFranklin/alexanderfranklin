83cb3c9acac14eaf4cbccb1ee6b4ffde0449b04f
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-29 23:16:41 -0700

    Remove Contact Page

--- src/views/Navbar/Navbar.js
+++ src/views/Navbar/Navbar.js
@@ -7,10 +7,9 @@ function Navbar() {
     <header className="header">
       <nav className="Navbar">
         <ul>
-          <Tab link="/" exact="true">Alexander Franklin</Tab>
+          <Tab link="/" exact="true">About Me</Tab>
           <Tab link="/resume">Resume</Tab>
           <Tab link="/projects">Projects</Tab>
-          <Tab link="/contact">Contact</Tab>
         </ul>
       </nav>
     </header>
