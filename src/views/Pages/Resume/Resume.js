00f94d64df188d6570a79b9eac27127d043985d0
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2020-06-30 14:44:55 -0700

    Style Resume

--- src/views/Pages/Resume/Resume.js
+++ src/views/Pages/Resume/Resume.js
@@ -5,29 +5,29 @@ function Resume() {
   return (
     <div className="Resume bubble">
       <div>
-        <h1>Technical Skills</h1>
+        <h2>Technical Skills</h2>
         Languages: Javascript, HTML5, CSS, Python, Applescript. <br/>
         Frameworks and Technologies: React, Redux, Git Version Control, AWS(Amplify), Node.js
       </div>
       <div>
-        <h1>Education</h1>
-        <b>Arizona State University Bachelor of Science in Software Engineering <span className="date">Est. 2024</span></b><hr/>
-        <b>SAT</b>
+        <h2>Education</h2>
+        Arizona State University Bachelor of Science in Software Engineering <span className="date">Est. 2024</span><hr/>
+        SAT
         <ul>
-          <li>Score: 1390</li>
-          <li>94th percentile</li>
+          <li><span>Score: 1390</span></li>
+          <li><span>94th percentile</span></li>
         </ul>
         <hr/>
-        <b>High School Diploma</b> <span className="date">Apr 2020</span>
+        High School Diploma <span className="date">Apr 2020</span>
         <ul>
-          <li>Homeschool</li>
+          <li><span>Homeschool</span></li>
         </ul>
       </div>
       <div>
-        <h1>Work Experience</h1>
-        <b>Harvest Homes</b> <span className="date">Sep 2018-Present</span> <br/>
+        <h2>Work Experience</h2>
+        Harvest Homes <span className="date">Sep 2018-Present</span> <br/>
         <ul>
-          <li>Maintenance</li>
+          <li><span>Maintenance</span></li>
         </ul>
       </div>
     </div>
