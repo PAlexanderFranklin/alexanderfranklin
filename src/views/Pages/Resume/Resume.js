941f36b6ab33b6f6879bf92da0f995d0ad2138ee
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2020-09-16 17:11:24 -0700

    Update resume

--- src/views/Pages/Resume/Resume.js
+++ src/views/Pages/Resume/Resume.js
@@ -6,15 +6,15 @@ function Resume() {
     <div className="Resume bubble">
       <div>
         <h2 className="no_top_margin">Technical Skills</h2>
-        Languages: Javascript, HTML<span className="number">5</span>, CSS, Python, Applescript, C++. <br/>
-        Frameworks and Technologies: React, Redux, Git Version Control, AWS(Amplify), Node.js
+        Languages: Javascript, HTML<span className="number">5</span>, CSS, Python, Lua, Applescript, C++. <br/>
+        Frameworks and Technologies: React, Redux, Git Version Control, AWS(Amplify), Node.js, Solar2D
       </div>
       <div>
         <h2>Education</h2>
         Arizona State University Bachelor of Science in Software Engineering <span className="date">Est. <span className="number">2024</span></span><hr/>
         SAT
         <ul>
-          <li><span className="bullet_small">Score: <span className="number">1390</span></span></li>
+          <li><span className="bullet_small">Score: <span className="number">1390/1600</span></span></li>
           <li><span className="bullet_small"><span className="number">94</span>th percentile</span></li>
         </ul>
         <hr/>
@@ -25,7 +25,11 @@ function Resume() {
       </div>
       <div>
         <h2>Work Experience</h2>
-        Harvest Homes <span className="date">Sep <span className="number">2018</span>-Present</span> <br/>
+        Contracting <span className="date">July <span className="number">2020</span>-Present</span> <br/>
+        <ul>
+          <li><span className="bullet_small">Lua</span></li>
+        </ul>
+        Harvest Homes <span className="date">Sep <span className="number">2018</span>-Sep <span className="number">2020</span></span> <br/>
         <ul>
           <li><span className="bullet_small">Maintenance</span></li>
         </ul>
