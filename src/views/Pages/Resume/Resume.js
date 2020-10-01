ede0c7ac4a6d9d5319981be4390dfe817a5f7d52
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2020-10-01 11:32:55 -0700

    Update resume

--- src/views/Pages/Resume/Resume.js
+++ src/views/Pages/Resume/Resume.js
@@ -6,12 +6,27 @@ function Resume() {
     <div className="Resume bubble">
       <div>
         <h2 className="no_top_margin">Technical Skills</h2>
-        Languages: Javascript, HTML<span className="number">5</span>, CSS, Python, Lua, Applescript, C++. <br/>
+        Languages: Javascript, HTML<span className="number">5</span>, CSS, PHP, Python, Lua, Applescript, C++. <br/>
         Frameworks and Technologies: React, Redux, Git Version Control, AWS(Amplify), Node.js, Solar2D
       </div>
       <div>
+        <h2>Work Experience</h2>
+        Contracting <span className="date">July <span className="number">2020</span>-Sep <span className="number">2020</span></span> <br/>
+        <ul>
+          <li><span className="bullet_small">Javascript</span></li>
+          <li><span className="bullet_small">PHP</span></li>
+          <li><span className="bullet_small">Lua</span></li>
+        </ul>
+        <hr/>
+        Harvest Homes <span className="date">Sep <span className="number">2018</span>-Sep <span className="number">2020</span></span> <br/>
+        <ul>
+          <li><span className="bullet_small">Maintenance</span></li>
+        </ul>
+      </div>
+      <div>
         <h2>Education</h2>
-        Arizona State University Bachelor of Science in Software Engineering <span className="date">Est. <span className="number">2024</span></span><hr/>
+        Arizona State University Bachelor of Science in Software Engineering <span className="date">Est. <span className="number">2024</span></span>
+        <hr/>
         SAT
         <ul>
           <li><span className="bullet_small">Score: <span className="number">1390/1600</span></span></li>
@@ -21,17 +36,6 @@ function Resume() {
         High School Diploma <span className="date">April <span className="number">2020</span></span>
         <ul>
           <li><span className="bullet_small">Homeschool</span></li>
-        </ul>
-      </div>
-      <div>
-        <h2>Work Experience</h2>
-        Contracting <span className="date">July <span className="number">2020</span>-Present</span> <br/>
-        <ul>
-          <li><span className="bullet_small">Lua</span></li>
-        </ul>
-        Harvest Homes <span className="date">Sep <span className="number">2018</span>-Sep <span className="number">2020</span></span> <br/>
-        <ul>
-          <li><span className="bullet_small">Maintenance</span></li>
         </ul>
       </div>
     </div>
