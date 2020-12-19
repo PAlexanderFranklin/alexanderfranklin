1423ca4b451a600c2f96d1ddec6d46a4a9e41258
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2020-12-18 21:49:52 -0800

    Add blank PantryAid component

---
+++ src/views/Pages/PantryAid/PantryAid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PantryAid.css';
+
+function PantryAid() {
+  return (
+    <div className="content">
+      <div className="PantryAid bubble">
+      </div>
+    </div>
+  );
+}
+
+export default PantryAid;
