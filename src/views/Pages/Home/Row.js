bebe8c00e1d6111fb0fe59c7e8dc9726e3730609
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2021-07-28 17:15:34 -0700

    Add delete button

--- src/views/Pages/Home/Row.js
+++ src/views/Pages/Home/Row.js
@@ -1,10 +1,17 @@
+import { API } from 'aws-amplify';
 import React from 'react';
 import './Row.css';
 
 function Row(props) {
+  function deleteMovie() {
+    API.del("moviesAPI", "/movies/object/" + props.title).then(
+      (e) => console.log(e)
+    )
+  }
   return (
     <div className="Row">
       <h2>{props.title}, {props.rating}, {props.genre}</h2>
+      <button onClick={deleteMovie}>Delete</button>
     </div>
   );
 }
