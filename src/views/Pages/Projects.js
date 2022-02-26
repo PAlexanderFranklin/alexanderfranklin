e81454f057d930f860947383ea5ed516e7a2ed5f
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2022-02-25 16:55:18 -0800

    Add peninsulabaptistchurch.org to Projects

--- src/views/Pages/Projects.js
+++ src/views/Pages/Projects.js
@@ -16,6 +16,38 @@ function Projects() {
     </Project>
   );
 
+  projectArray.push(
+    <Project title="peninsulabaptistchurch.org">
+      This is another website I have designed and built. The interesting
+      part of it is the "Sermons" page. It uses SQL from within Javascript
+      to query an SQLite database that is sent to the frontend. The queries
+      support filtering by various categories and searching. There are queries
+      for determining page numbers and for listing attributes available to
+      filter by.
+      <p>
+        I designed the database myself. It uses multiple tables to support
+        one to many relationships between the data it contains. On the
+        maintenance side of the database, I wrote a Python script to populate
+        the database with metadata while creating the audio data itself.
+      </p>
+      <p>
+        The audio itself is stored in the mp3 format using a technology called
+        Skynet. Skynet stores files in a decentralized network in a manner that
+        is accessible from the general internet. The SQLite database stores links
+        to the audio files, and the frontend then retrieves them from Skynet
+        after querying the database.
+      </p>
+      <hr/>
+      <a
+      className="alex_button project_button"
+      href="https://peninsulabaptistchurch.org/sermons"
+      draggable="false"
+      target="_blank"
+      rel="noopener noreferrer">
+        Link to the Sermons Page
+      </a>
+    </Project>
+  );
 
   projectArray.push(
     <Project title="Gravity Rotation Simulator">
@@ -26,7 +58,7 @@ function Projects() {
       and decides when and where to render objects to the scratch system. It has velocity, acceleration, and
       basic air resistance. It also has basic collision detection. <hr/>
       <a
-      className="alex_button scratch"
+      className="alex_button project_button"
       href="https://scratch.mit.edu/projects/212668429/"
       draggable="false"
       target="_blank"
