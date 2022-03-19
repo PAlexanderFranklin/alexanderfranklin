effb705e8c074b02e3f68a33b307163fb9e1f767
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2022-03-19 10:23:07 -0700

    Add Yap and Case Study

--- src/views/Pages/Projects.js
+++ src/views/Pages/Projects.js
@@ -18,20 +18,22 @@ function Projects() {
 
   projectArray.push(
     <Project title="peninsulabaptistchurch.org">
-      This is another website I have designed and built. The interesting
-      part of it is the "Sermons" page. It uses SQL from within Javascript
-      to query an SQLite database that is sent to the frontend. The queries
-      support filtering by various categories and searching. There are queries
-      for determining page numbers and for listing attributes available to
-      filter by.
+      <p>
+        This is another website I have designed and built. The interesting
+        part of it is the "Sermons" page. It uses SQL from within Javascript
+        to query an SQLite database that is sent to the frontend. The queries
+        support filtering by various categories and searching. There are queries
+        for determining page numbers and for listing attributes available to
+        filter by.
+      </p>
       <p>
         I designed the database myself. It uses multiple tables to support
         one to many relationships between the data it contains. On the
         maintenance side of the database, I wrote a Python script to populate
-        the database with metadata while creating the audio data itself.
+        the database with metadata while processing the audio data itself.
       </p>
       <p>
-        The audio itself is stored in the mp3 format using a technology called
+        The audio is stored in the mp3 format using a technology called
         Skynet. Skynet stores files in a decentralized network in a manner that
         is accessible from the general internet. The SQLite database stores links
         to the audio files, and the frontend then retrieves them from Skynet
@@ -44,7 +46,65 @@ function Projects() {
       draggable="false"
       target="_blank"
       rel="noopener noreferrer">
-        Link to the Sermons Page
+        Go to the Sermons Page
+      </a>
+    </Project>
+  );
+
+  projectArray.push(
+    <Project title="Bike Rental Analysis">
+      <p>
+        At the end of the Google Data Analytics Certificate Course, I completed
+        this case study to demonstrate some of the skills I had gained
+        during the course. I was given a dataset to analyze and a question to answer.
+        The dataset consisted of about 5.5 million rows, and since excel didn't
+        like that very much, I combined pieces of the dataset into a single
+        SQLite database, used SQL to explore and clean the data, and used R
+        to visualize the data both for analysis and later for presenting
+        conclusions. During the analysis, I was sure to make a note any time
+        I removed data for being irrelevent, giving the reasoning for it.
+        I also noted when questions were outside the scope of the data, providing
+        a possible explanation, but saying further research would be required to
+        properly answer a question.
+      </p>
+      <hr/>
+      <a
+      className="alex_button project_button"
+      href="https://docs.google.com/document/d/13eJaQz383PAIqVBW8PHl4kCzlvMFG7JhnI8_2tSqfAY/edit?usp=sharing"
+      draggable="false"
+      target="_blank"
+      rel="noopener noreferrer">
+        Read the Full Case Study
+      </a>
+    </Project>
+  );
+
+  projectArray.push(
+    <Project title="Yap">
+      <p>
+        This is a React application that I created for a weekend hackathon hosted
+        by Skynet. It allows you to record audio and upload it to be publicly
+        available on Skynet, and then gives you a link that you can share.
+        It uses MySky, a Skynet Feature, for authentication. You can use Yap
+        without signing in, but MySky stores the audio links so you can find
+        them later.
+      </p>
+      <p>
+        Yap is hosted on Skynet rather than a traditional server and DNS model.
+        The yasp.hns handshake name points to the app (yap.hns was taken),
+        which means that it can be found at yasp.hns.exampleportal.com.
+        This works for most Skynet portals, although as of the time of writing
+        there are only a few public portals online. The link below takes you
+        to yasp.hns.fileportal.org.
+      </p>
+      <hr/>
+      <a
+      className="alex_button project_button"
+      href="https://yasp.hns.fileportal.org"
+      draggable="false"
+      target="_blank"
+      rel="noopener noreferrer">
+        Go to Yap
       </a>
     </Project>
   );
@@ -54,16 +114,19 @@ function Projects() {
       <p>
         Desktop Only.
       </p>
-      I made this using scratch, a visual block language. It uses its own coordinate system,
-      and decides when and where to render objects to the scratch system. It has velocity, acceleration, and
-      basic air resistance. It also has basic collision detection. <hr/>
+      <p>
+        I made this using scratch, a visual block language. It uses its own coordinate system,
+        and decides when and where to render objects to the scratch system. It has velocity, acceleration, and
+        basic air resistance. It also has basic collision detection.
+      </p>
+      <hr/>
       <a
       className="alex_button project_button"
       href="https://scratch.mit.edu/projects/212668429/"
       draggable="false"
       target="_blank"
       rel="noopener noreferrer">
-        Link to Gravity Simulator
+        Go to Gravity Simulator
       </a>
     </Project>
   );
